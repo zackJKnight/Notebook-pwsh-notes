baf36cdc90444750f472bf84ae36b6417042442e
Zachary Knight <zack.knight@seicdevops.com>
2021-06-10 08:28:59 -0500

    update exercises

--- Exercises.ps1
+++ Exercises.ps1
@@ -6,4 +6,18 @@ $aliases = @(Get-ChildItem Alias:\)
 $aliases[[int](Get-Random -Maximum $aliases.Length)] | Select-Object Name
 
 
-# 2. Find
+# 2. Find a command in the Microsoft.PowerShell.Utility module that can generate a GUID. Generate a GUID.
+# Hint
+Get-Command
+
+# 3. If today is an even day, Write 'We're even!' to the console. Otherwise, write 'We got a score to settle'
+
+# 4. Start the notepad process and then find the path to the executable file that is running.
+
+# 5. If you're on Windows, measure how many services are running
+# Hint 
+Get-Service 
+and 
+Measure-Object
+
+# 6. Given the Web-Data script, change it to show headlines from the previous month
