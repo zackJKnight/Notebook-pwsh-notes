fee97100479bdd86ba39491bde6fe4464528cc95
zachary knight <zachary.j.knight@gmail.com>
2021-06-10 13:44:51 -0500

    post talk notes

--- Exercises.ps1
+++ Exercises.ps1
@@ -10,9 +10,11 @@ $aliases[[int](Get-Random -Maximum $aliases.Length)] | `
 
 # 2. Find a command in the Microsoft.PowerShell.Utility 
 #    module that can generate a GUID. Generate a GUID.
-
+Where-Object { $_.Prop -like 'myTerm'}
 # Hint
 Get-Command
+# -or
+Get-Help
 
 # 3. If today is an even day, Write 'We're even!' to the console. 
 #    Otherwise, write 'We got a score to settle'
