d43ff92fec576c2a2e420825cbd205c9a4573599
zachary knight <zachary.j.knight@gmail.com>
2021-06-10 11:34:05 -0500

    cleanup presentation for zoomed view

--- Exercises.ps1
+++ Exercises.ps1
@@ -1,23 +1,31 @@
 
-# 1. Discover properties and use Where-Object to find the proper commandlet name of the given alias.
+# 1. Discover properties and use Where-Object 
+#    to find the proper commandlet name of the given alias.
 
 $aliases = @(Get-ChildItem Alias:\)
 
-$aliases[[int](Get-Random -Maximum $aliases.Length)] | Select-Object Name
+$aliases[[int](Get-Random -Maximum $aliases.Length)] | `
+    Select-Object Name
 
 
-# 2. Find a command in the Microsoft.PowerShell.Utility module that can generate a GUID. Generate a GUID.
+# 2. Find a command in the Microsoft.PowerShell.Utility 
+#    module that can generate a GUID. Generate a GUID.
+
 # Hint
 Get-Command
 
-# 3. If today is an even day, Write 'We're even!' to the console. Otherwise, write 'We got a score to settle'
+# 3. If today is an even day, Write 'We're even!' to the console. 
+#    Otherwise, write 'We got a score to settle'
 
-# 4. Start the notepad process and then find the path to the executable file that is running.
+# 4. Start the notepad process and then find the path 
+#    to the executable file that is running.
 
 # 5. If you're on Windows, measure how many services are running
-# Hint 
+#    Resist the urge to look at the hint
+#    Hint 
 Get-Service 
 and 
 Measure-Object
 
-# 6. Given the Web-Data script, change it to show headlines from the previous month
+# 6. Given the Web-Data script, change it to show headlines 
+#    from the previous month
